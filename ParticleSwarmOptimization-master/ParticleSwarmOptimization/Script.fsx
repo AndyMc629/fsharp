b87a25354095b787f03ec8caf89cb910cd0ba249
amcmahon-streamba <andrewpmcmahon629@gmail.com>
2018-04-01 11:24:59 +0100

    Fsharp stuff first commit

---
+++ ParticleSwarmOptimization-master/ParticleSwarmOptimization/Script.fsx
@@ -0,0 +1,17 @@
+﻿// SEE http://www.danielslater.net/2015/05/particle-swarm-optimization-in-f.html
+#load "ParticleSwarmOptimization.fs"
+open ParticleSwarmOptimization
+
+let target_point = [23.0;54.0]
+let loss_func (x : list<float>) =  x |> List.map2 (fun x y -> sin 1.0/x-sin 1.0/y) target_point |> List.sum |> abs
+let random = new System.Random()
+let initial_weights = seq{ while true do yield [random.NextDouble()*1000.0;random.NextDouble()*1000.0]};
+
+let args = Args(inertia_weight=[0.8; 0.8], particles=10, success_threshold=0.01, iterations = 10);
+let mutable (global_best_params, global_best_loss, particles) = execute args loss_func initial_weights;;
+
+//running multiple loops
+for i in 0 .. 10 do
+    printfn "Particles %A" particles
+    printfn "Best loss %A" global_best_loss
+    (global_best_params, global_best_loss, particles) = execute args loss_func (particles |> Seq.map (fun x -> x.Parameters |> Seq.toList));;
---
+++ time-series/TimeSeriesSimple.fsx
@@ -0,0 +1,55 @@
+#load "packages/FsLab/Themes/DefaultWhite.fsx"
+#load "packages/FsLab/FsLab.fsx"
+
+#r "MathNet.Numerics.dll"
+
+open System
+open Deedle
+open FSharp.Data
+open XPlot.GoogleCharts
+open XPlot.GoogleCharts.Deedle
+open MathNet.Numerics
+
+let tradingDaysInAYear = 252
+
+
+let randomWalk dailyReturn dailyStandardDeviation (days:int) =
+    let steps = MathNet.Numerics.Generate.Normal(days, dailyReturn, dailyStandardDeviation)
+    let prices = Array.scan (+) 1.0 steps
+    prices
+
+
+let annualReturn = 0.05
+let dailyReturn = Math.Pow((1.0+annualReturn), (1.0/(float tradingDaysInAYear)))-1.0
+let dailyStandardDeviation = 0.10/(sqrt (float tradingDaysInAYear))
+
+let plotWalks N =
+    let walks = [for i in 1..N ->
+                 randomWalk dailyReturn dailyStandardDeviation (1*tradingDaysInAYear)]
+
+    let average = [for day in 1..tradingDaysInAYear do
+                       let values = [for i in 1..N -> walks.[i-1].[day-1]]
+                       yield (List.average values)]
+        
+    let averageChart =
+        [average |> Array.ofList |> Array.indexed]
+        |> Chart.Line
+        |> Chart.WithLabel "Average"
+        |> Chart.WithOptions (Options(lineWidth=5))
+
+    let combinedSeries =
+        seq { yield! walks; yield (average |> Array.ofList) }
+        |> Seq.map Array.indexed
+        |> List.ofSeq
+    
+    let isAverageIndicator = [for i in 1..N+1 -> (i > N)]
+    let widths = [for isAvg in isAverageIndicator -> if isAvg then 5 else 1]
+    let options = Options(series=[|for w in widths -> Series(lineWidth=w)|])
+
+    combinedSeries
+    |> Chart.Combo
+    |> Chart.WithOptions options
+    |> Chart.WithTitle "Random Walks"
+
+
+plotWalks 10;;
